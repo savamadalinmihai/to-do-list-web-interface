fb50d96e257709b50ccb47a766f1f4a8fcf70079
savamadalinmihai <savamadalinmihai@gmail.com>
2020-02-17 17:54:40 +0200

    displaying tasks from api. works with postman

--- js/tasks.js
+++ js/tasks.js
@@ -8,16 +8,35 @@ window.ToDoList = {
             method: "GET"
         }).done(function (response) {
             console.log(response);
+
+            ToDoList.displayTasks(JSON.parse(response));
         })
     },
 
     getTaskRow: function (task) {
+        // (...) this is the spread operator, that helps us when we want to read multiple values from an array
+        let formattedDeadline = new Date(...task.deadline).toLocaleDateString();
+
+        // ternary operator (if else written in one line)
+        let checkedAttribute = task.done ? "checked" : "";
+
+
         return `<tr>
             <td>${task.description}</td>
-            <td>${task.deadline}</td>
-            <td><input type="checkbox" data-id=1 class="mark-done"/>
+            <td>${formattedDeadline}</td>
+            <td><input type="checkbox" data-id=1 class="mark-done" ${ checkedAttribute}/>
             <td><a href="#" data-id=1 class="delete-task"><i class="fas fa-trash-alt"></i></a></td>
         </tr>`
+    },
+
+    displayTasks: function (tasks) {
+        // javascript is weak-typed (we can store anything in a variable)
+        // java is strong-typed (we can not store anything we want in a variable).
+        var tableBody = '';
+
+        tasks.forEach(task => tableBody += ToDoList.getTaskRow(task));
+
+        $("#tasks-table tbody").html(tableBody);
     }
 
 };
