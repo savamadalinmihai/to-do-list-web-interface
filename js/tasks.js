99ffb8de16fd344467902f19b9faeb4e76f471fa
savamadalinmihai <savamadalinmihai@gmail.com>
2020-02-17 21:16:56 +0200

    created methods to send create task request to api

--- js/tasks.js
+++ js/tasks.js
@@ -13,6 +13,34 @@ window.ToDoList = {
         })
     },
 
+    createTask: function () {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
+
+        let requestBody = {
+          description: descriptionValue,
+          deadline: deadlineValue
+        };
+
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "POST",
+            //content type, also known as mime type
+            contentType: "application/json",
+            // json stringify get a string from a JSON object
+            // json parse to get a JSON object FROM a string.
+            data: JSON.stringify(requestBody)
+            }).done(function () {
+             ToDoList.getTasks();
+        })
+    },
+
+    updateTask: function() {
+        let description;
+        let deadline;
+        let done;
+    },
+
     getTaskRow: function (task) {
         // (...) this is the spread operator, that helps us when we want to read multiple values from an array
         let formattedDeadline = new Date(...task.deadline).toLocaleDateString();
@@ -24,7 +52,7 @@ window.ToDoList = {
         return `<tr>
             <td>${task.description}</td>
             <td>${formattedDeadline}</td>
-            <td><input type="checkbox" data-id=1 class="mark-done" ${ checkedAttribute}/>
+            <td><input type="checkbox" data-id=1 class="mark-done" ${checkedAttribute}/>
             <td><a href="#" data-id=1 class="delete-task"><i class="fas fa-trash-alt"></i></a></td>
         </tr>`
     },
@@ -37,8 +65,18 @@ window.ToDoList = {
         tasks.forEach(task => tableBody += ToDoList.getTaskRow(task));
 
         $("#tasks-table tbody").html(tableBody);
+    },
+
+    bindEvents: function () {
+        //this function connects the functions we created (create/update/etc) to the events (click/submit/etc)
+        $("#new-task-form").submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createTask();
+        })
     }
 
 };
 
 ToDoList.getTasks();
+ToDoList.bindEvents();
